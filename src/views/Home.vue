7681480a74b01b4a7c4fda057a43b81980b2fe9b
wilsunson <839785143@qq.com>
2019-04-26 18:08:58 +0800

    引入新的富文本

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,38 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-    wtfffffffffff
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>wtfffffffffff
     <div class="home_text">
-<ckeditor :editor="editor" v-model="editorData" :config="editorConfig"  ></ckeditor>
+      <ckeditor :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
     </div>
-    
+    <div>
+      <editor :editorContent.sync="editorData" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic"; 
-import '@ckeditor/ckeditor5-build-classic/build/translations/zh-cn.js'  // 引入中文包
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import Editor from "./TinymceEditor.vue";
+import "@ckeditor/ckeditor5-build-classic/build/translations/zh-cn.js"; // 引入中文包
 
 @Component({
   components: {
-    HelloWorld
+    HelloWorld,
+    Editor
   }
 })
 export default class Home extends Vue {
   editor = ClassicEditor;
   editorData = `<p>111111111111111111111<p>`;
   editorConfig = {
-    language: 'zh-cn',//用中文
+    language: "zh-cn" //用中文
   };
 }
 </script>
 <style>
-.home_text{
+.home_text {
   text-align: left;
 }
 </style>
