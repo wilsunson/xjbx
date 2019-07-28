67fd725b09ccb18d998beec13cfecd471e9e9aef
wilsunson <839785143@qq.com>
2019-07-28 15:10:06 +0800

    自定义步骤条组件。。

--- src/views/input.vue
+++ src/views/input.vue
@@ -1,18 +1,29 @@
 <template>
-    <div>
-        {{num1}}
-        <el-button type="warning" @click="getNumber">get a random number</el-button>
-    </div>
+  <div>
+    {{num1}}
+    <el-button type="warning" @click="getNumber">get a random number</el-button>
+    <Steps :Steps="registSpets" :SetData="SetData" />
+    <el-button type="primary" @click="registSpets++">+++</el-button>
+    {{registSpets}}
+    <el-button type="danger" @click="registSpets--">---</el-button>
+  </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
-@Component
-export default class Input extends Vue{
-    num1:number=0;
-    getNumber(){
-        this.num1=Math.ceil(Math.random()*10); // Matg.ceil向上取整
-    }
+import { Component, Prop, Vue } from "vue-property-decorator";
+import Steps from "../components/Steps.vue";
+@Component({
+  components: {
+    Steps
+  }
+})
+export default class Input extends Vue {
+  num1: number = 0;
+  registSpets = 1;
+  SetData = ["用户信息", "魔童哪吒", "齐天大圣", "赤脚大仙", "我爱中国"];
+  getNumber() {
+    this.num1 = Math.ceil(Math.random() * 10); // Matg.ceil向上取整
+  }
 }
 </script>
 
